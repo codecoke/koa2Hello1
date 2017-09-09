90117bbaeb4f7b1989d414075308d4729a35d6ef
codecoke <w@codecoke.com>
2017-09-09 18:34:49 +0800

    edit app async list

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async (ctx,next) => {
-  console.log('async 1');
+  console.log('async 1 begin -----');
   await next();
-  console.log('async end');
+  console.log('async 1 end -----\n');
   ctx.response.type = 'text/html';
   ctx.response.body = '<h1>hello koa2!</h1>';
 });
 
 app.use(async (ctx,next) => {
-  console.log(`'async 2 ${ctx.request.method} ${ctx.request.url}'`);
+  console.log(` async 2 begin ${ctx.request.method} ${ctx.request.url}`);
   await next();
-  console.log('async 2.2');
+  console.log(' async 2 end');
 });
 
 app.use(async (ctx,next) => {
-  console.log('async 3.1');
+  console.log('   async 3 begin');
   await next();
-  console.log('async 3.2');
+  console.log('   async 3 end');
 });
 
 
-app.listen(3000);
-console.log('[demo] app start at port 3000...!');
+app.listen(3000,(ctx) => {
+  console.log('[demo] app start at port 3000...!');
+});
